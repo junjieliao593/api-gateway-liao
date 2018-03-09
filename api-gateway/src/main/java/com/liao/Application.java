ec1f4a8c969f99dff356bb636f941e6cc8487c0d
junjieliao593 <junjieliao@139.com>
2018-03-09 15:18:45 +0800

    初始提交修改

--- api-gateway/src/main/java/com/liao/Application.java
+++ api-gateway/src/main/java/com/liao/Application.java
@@ -3,6 +3,9 @@ package com.liao;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.liao.filter.AccessFilter;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -10,6 +13,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 
 }
