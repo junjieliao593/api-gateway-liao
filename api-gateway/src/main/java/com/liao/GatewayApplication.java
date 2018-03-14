6edb6e6fa520abc2c19bc3d97d14f57a0a770053
junjieliao593 <junjieliao@139.com>
2018-03-14 13:52:56 +0800

    成功搭建分布式网关配置中心

--- api-gateway/src/main/java/com/liao/GatewayApplication.java
+++ api-gateway/src/main/java/com/liao/GatewayApplication.java
@@ -1,6 +1,5 @@
 package com.liao;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
--- config-server-git/src/main/java/com/liao/ConfigApplication.java
+++ config-server-git/src/main/java/com/liao/ConfigApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
-@SpringBootApplication
+@EnableEurekaClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
